fbeef09226de18ed977bbf84f077e99bdeeb0ca5
Anthony Givans <givansanthony702@gmail.com>
2022-05-21 19:51:09 -0400

    updated singly-ll.py

--- singly-ll.py
+++ singly-ll.py
@@ -45,6 +45,70 @@ class LinkedList:
         new_node.next = prev_node.next
         prev_node.next = new_node
 
+    def len_iterative(self):
+
+        count = 0
+
+        cur_node = self.head
+
+        while cur_node:
+            count += 1
+            cur_node = cur_node.next
+
+        return count
+
+    def len_recursive(self, node):
+
+        if node is None:
+            return 0
+
+        return 1 + self.len_recursive(node)
+
+    def delete_node(self, key):
+
+        cur_node = self.head
+
+        if cur_node and cur_node.data == key:
+            self.head = cur_node.next
+            cur_node = None
+            return
+
+        prev = None
+
+        while cur_node and cur_node.data != key:
+            prev = cur_node
+            cur_node = cur_node.next
+
+        if cur_node is None:
+            return
+
+        prev.next = cur_node.next
+        cur_node = None
+
+    def delete_node_at_pos(self, pos):
+
+        if self.head:
+            cur_node = self.head
+
+            if pos == 0:
+                self.head = cur_node.next
+                cur_node = None
+                return
+
+            prev = None
+            count = 0
+
+            while cur_node and count != pos:
+                prev = cur_node
+                cur_node = cur_node.next
+                count += 1
+
+            if cur_node is None:
+                return
+
+            prev.next = cur_node.next
+            cur_node = None
+
     def print_ll(self):
         cur_node = self.head
         while cur_node:
